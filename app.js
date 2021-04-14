88efb5983edd70c8687febd0521be2455d500afb
jose Andres <jcastanedaforno@gmail.com>
2021-04-14 12:43:12 -0600

    =add and remove a note

--- app.js
+++ app.js
@@ -1,10 +1,66 @@
-const getNotes = require('./utils.js');
-const validator = require('validator');
-const chalk = require('chalk');
+const notes = require('./utils.js');
+const yargs = require('yargs');   //Yargs parses the command line parameters
 
-const msg = getNotes();
+// working with arguments in command line
+// console.log(process.argv);   //Argument Vector (all the arguments provided) 1.nodejs executable 2.file path, 3.arguments
+// console.log(yargs.argv);
+// const command = process.argv[2];
 
-console.log(msg);
+yargs.version = "1.1.0";
+// Create command
+yargs.command({
+  command: 'add',
+  describe: 'add a new note',
+  builder:{ 
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'body of the new note',
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
 
-console.log(validator.isEmail('jcastanedaforno@asdf.com'));
-console.log(chalk.green('Succes!'));
+// Remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove a new note',
+  builder: {
+    title: {
+      describe: "The note title to remove",
+      require: true,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+  }
+});
+
+// List command
+yargs.command({
+  command: 'read',
+  describe: 'Reading a note',
+  handler: function () {
+    console.log("reading a note");
+  }
+});
+
+// Delete command
+yargs.command({
+  command: 'delete',
+  describe: 'deleting a note',
+  handler: function () {
+    console.log("delet a note");
+  }
+});
+
+// Parse all the arguments
+yargs.parse();
--- utils.js
+++ utils.js
@@ -1,5 +1,64 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
 const getNotes = function () {
   return 'Yout notes...';
 }
 
-module.exports = getNotes;
+// Add a note to the array of lists
+const addNote = function (title, body) {
+  // Get all the existing notes
+  const notes = loadNotes();
+  // Validate that the title doesnt exist yet
+  const existing = notes.filter(elm => {
+    return elm.title === title;
+  })
+  // Add the new one
+  if(existing.length == 0){
+    notes.push({
+      title: title, 
+      body: body
+    });
+  } else { 
+    console.log("Note already exist");
+  }
+  // Save the notes 
+  saveNotes(notes);
+}
+
+const removeNote = function(title){ 
+  // Get all the existing notes
+  const notes =  loadNotes();
+  notes.forEach((element, index, object) => {
+    if(element.title == title){
+      object.splice(index,1);
+      console.log(chalk.green("Note Deleted!"));
+    }
+  });
+  // Save the notes 
+  saveNotes(notes);
+}
+
+// Save a the json array of notes into the file
+const saveNotes = function(notes){
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+}
+
+// Loads all the notes from the notes.json file
+const loadNotes =  function () {
+  try {
+    const dataBufer = fs.readFileSync('notes.json');
+    const dataJSON = dataBufer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+}
+
+// Export the funtions
+module.exports = {
+  'addNote': addNote,
+  'getNotes': getNotes,
+  'removeNote': removeNote
+}
