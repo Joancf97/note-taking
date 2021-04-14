998dd2f7e1d096a85ab5c652677ae9aaa073be67
jose Andres <jcastanedaforno@gmail.com>
2021-04-14 02:48:19 -0600

    using npm

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const getNotes = require('./utils.js');
-
+const validator = require('validator');
+const chalk = require('chalk');
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(validator.isEmail('jcastanedaforno@asdf.com'));
+console.log(chalk.green('Succes!'));
